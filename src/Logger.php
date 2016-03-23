c9c9cbd5b1b0774c47b46fbc2bb7ca68f1d5d3a2
Martyn Ranyard <martyn@ranyard.info>
2016-03-23 21:20:02 +0000

    Revert "fix log display for cutting times > 1hr"
    This reverts commit 2ad7f2fa06f3714fc3b3c44c263417573227b105.
    
    Did not run after this merge!

--- src/Logger.php
+++ src/Logger.php
@@ -61,22 +61,20 @@ Class Logger {
                 $thisresult = array('inducteeuid'=>$result['inducteeuid'],'starttime'=>$result['starttime'],'endtime'=>$result['endtime']);
                 $phpendtime = strtotime($result['endtime']);
                 $phpstarttime = strtotime($result['starttime']);
-                $totalsecs = $phpendtime - $phpstarttime;
+                $secs = $phpendtime - $phpstarttime;
                 $mysqlsecs = $result['seconds'];
                 $thisresult['seconds'] = $secs;
                 $this->app->log->debug($secs);
-
-                list($hours,$mins,$secs) = explode(',',date('H,i,s',$secs),3);
-
                 $thisresult['nicetime'] = sprintf('%u seconds',$secs);
-
-                if ($totalsecs >= 60) {
-                       $thisresult['nicetime'] = sprintf('%u minutes and %u seconds',$mins,$secs);
-                }
-                if ($totalsecs >= 3600) {
+                if ($secs > 60) {
+                   list($mins,$secs) =explode(',',date('i,s',$secs),2);
+                   if ($mins > 60) {
+                       list($hours,$mins,$secs) = explode(',',date('H,i,s',$secs),3);
                        $thisresult['nicetime'] = sprintf('%u hours, %u minutes and %u seconds',$hours,$mins,$secs);
-                }
-
+                   } else {
+                       $thisresult['nicetime'] = sprintf('%u minutes and %u seconds',$mins,$secs);
+                   }
+		}
                 $results[] = $thisresult;
             }
             return $results;
